bad42d60a0da3c64cd608dd49bc7326a5c7da008
Mudassir <mudassir.asghar.info@gmail.com>
2024-09-29 22:31:39 +0500

    added grocery item response and request DTOs

--- Sources/SampleGroceryAppSharedDTO/GroceryItemResponseDTO.swift
+++ Sources/SampleGroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -12,11 +12,15 @@ public struct GroceryItemResponseDTO: Codable {
     public let title: String
     public let price: Double
     public let quantity: Int
+    public var error: Bool
+    public var reason: String?
 
-    public init(id: UUID, title: String, price: Double, quantity: Int) {
+    public init(id: UUID, title: String, price: Double, quantity: Int, error: Bool = false, reason: String? = nil) {
         self.id = id
         self.title = title
         self.price = price
         self.quantity = quantity
+        self.error = error
+        self.reason = reason
     }
 }
